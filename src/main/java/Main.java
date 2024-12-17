43c91448a536418bd62e9f943e7ac945297219ce
Daniil <7102771@gmail.com>
2024-12-17 23:54:12 +0300

    JavaProject StringArray::modified

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        StringArray stringArray = new StringArray();
+        stringArray.add("Это первая строка!");
+        stringArray.add("Это вторая стока - самая длинная из всех!");
+        stringArray.add("Это 3 строка!");
+
+        System.out.println("Максимальная строка по длине: " + stringArray.getMaxLengthString());
+        System.out.println("Средняя длина строк: " + stringArray.getAverageLength());
+    }
 }
--- src/main/java/StringArray.java
+++ src/main/java/StringArray.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Collections;
+class StringArray {
+    private ArrayList<String> strings;
 
-public class StringArray {
+    public StringArray() {
+        strings = new ArrayList<>();
+    }
+
+    public void add(String string) {
+        strings.add(string);
+        Collections.sort(strings, (a, b) -> Integer.compare(a.length(), b.length()));
+    }
+
+    public String getMaxLengthString() {
+        if (strings.isEmpty()) {
+            return null;
+        }
+        String maxString = strings.get(0);
+        for (String str : strings) {
+            if (str.length() > maxString.length()) {
+                maxString = str;
+            }
+        }
+        return maxString;
+    }
+
+    public double getAverageLength() {
+        if (strings.isEmpty()) {
+            return 0;
+        }
+        int totalLength = 0;
+        for (String str : strings) {
+            totalLength += str.length();
+        }
+        return (double) totalLength / strings.size();
+    }
 }
